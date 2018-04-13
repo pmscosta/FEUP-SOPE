ccccfbe2d1882fd9040fb56c047cb9772359a17b
Unknown <up201605339@fe.up.pt>
2018-04-13 22:41:03 +0100

    Auto stash before merge of "master" and "origin/master"

--- Utilities.c
+++ Utilities.c
@@ -14,30 +14,39 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/time.h>
 #include <errno.h>
 #include <ctype.h>
 #include <stdbool.h>
 
 #define MAX_BUFFER_LENGHT 1024
 #define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
 #define CLOSE 0
 #define OPEN 1
 
-typedef struct{
-  bool ignoreCase;
-  bool showOnlyFileName;
-  bool showLineNumber;
-  bool showNumberOfLines;
-  bool completeWord;
-  bool recursive;
-}options;
+typedef struct {
+    bool ignoreCase;
+    bool showOnlyFileName;
+    bool showLineNumber;
+    bool showNumberOfLines;
+    bool completeWord;
+    bool recursive;
+} options;
 
+
+/*
+ * store program initial time
+ */
+struct timeval t_init;
+struct timeval t_end;
+
+
+double getTime(){
+    gettimeofday(&t_end, NULL);
+    double time = (double) (t_end.tv_usec - t_init.tv_usec) / 1000000 + (double) (t_end.tv_sec - t_init.tv_sec);
+    time *= 1000;
+    return time;
+}
 
 void parent_sigint_handler(int signo) {
     int repeat = 1;
@@ -83,14 +92,32 @@ void parent_sigchld_handler(int signo) {
 
 char *createFileMessage(double time, int pid, char *filename) {
 
-    char *message = NULL;
-
-
-    if (asprintf(&message, "%f - %d %s\n", time, pid, filename) == -1) {
-        fprintf(stderr, "Unable to create log message to file\n");
+    if (fwrite(message, strlen(message), 1, fp) == 0) {
+        fprintf(stderr, "An error occurred while saving to the file\n");
         exit(1);
     }
 
+    fclose(fp);
+}
+
+char *createFileMessage(double time, int pid, char *filename, int mode) {
+
+    char *message = NULL;
+
+    char *action = NULL;
+
+    int n;
+
+    if (mode == OPEN) {
+        n = asprintf(&action, "Opened %s", filename);
+    } else {
+        n = asprintf(&action, "Closed [%s]", filename);
+    }
+
+    if (n == -1 || asprintf(&message, "%.2f - %d %s\n", time, pid, action) == -1) {
+        fprintf(stderr, "Unable to create log message to file\n");
+        exit(1);
+    }
 
     return message;
 
@@ -104,32 +131,31 @@ bool completeWord(char* buffer, char* pattern,char *word_location){
 
     if (word_location > buffer) {
 
-        if(isalnum(*(word_location-1)))
+        if (isalnum(*(word_location - 1)))
             return false;
 
         /*
          * checking if the word is in the middle of the sentence
          * so that we don't access invalid memory
          */
-        if(((word_location + strlen(pattern)) < ( buffer + strlen(buffer)))) {
+        if (((word_location + strlen(pattern)) < (buffer + strlen(buffer)))) {
 
             if (!isalnum(*(word_location + strlen(pattern)))) {
-              return true;
+                return true;
             }
 
-        }
-        else{
-              return true;
+        } else {
+            return true;
         }
 
-    }else if(!isalnum(*(word_location + strlen(pattern)))){
-      return true;
+    } else if (!isalnum(*(word_location + strlen(pattern)))) {
+        return true;
     }
 
-  return false;
+    return false;
 }
 
-void findPatternInput(char *pattern, options* op) {
+void findPatternInput(char *pattern, options *op) {
 
     char *buffer;
 
@@ -150,12 +176,13 @@ void findPatternInput(char *pattern, options* op) {
         }
 
         if ((pattern_location = strstr(buffer, pattern)) != NULL) {
-          if (((pattern_location = strstr(buffer, pattern))!=NULL && !op->ignoreCase) || (op->ignoreCase && (pattern_location = strcasestr(buffer, pattern)) != NULL)) {
+            if (((pattern_location = strstr(buffer, pattern)) != NULL && !op->ignoreCase) ||
+                (op->ignoreCase && (pattern_location = strcasestr(buffer, pattern)) != NULL)) {
 
-            if(!op->completeWord || completeWord(buffer,pattern, pattern_location) ){
-                  printf(ANSI_COLOR_RED "%s" ANSI_COLOR_RESET  "\n", buffer);
+                if (!op->completeWord || completeWord(buffer, pattern, pattern_location)) {
+                    printf(ANSI_COLOR_RED "%s" ANSI_COLOR_RESET  "\n", buffer);
+                }
             }
-          }
 
         }
 
@@ -164,77 +191,70 @@ void findPatternInput(char *pattern, options* op) {
     free(buffer);
 
 }
+
 /*
  * search for the pattern, taking in consideration the case
  *
  */
 
-void findPatternFile(char *filename, char *pattern, options * op) {
+void findPatternFile(char *filename, char *pattern, options *op, const char *env) {
 
-   FILE *file = fopen(filename, "r");
+    FILE *file = fopen(filename, "r");
 
-   if (file == NULL) {
-       fprintf(stderr, "could not open: %s\n", filename);
-       exit(2);
-   }
-
-   char *buffer = NULL;
-
-   size_t size = 0;
-
-   char *word_location;
-
-   size_t n = 0;
-
-   int nLines = 0;
-
-   int lineCounter=0;
-
-
-   if(op->showOnlyFileName) printf("%s\n", filename);
-
-   while ((n = getline(&buffer, &size, file)) != -1) {
-       ++nLines;
-
-         if (((word_location = strstr(buffer, pattern))!=NULL && !op->ignoreCase) || (op->ignoreCase && (word_location = strcasestr(buffer, pattern)) != NULL)) {
-
-           if(!op->completeWord || completeWord(buffer,pattern, word_location) ){
-             if(op->showNumberOfLines) lineCounter++;
-             else {
-               if(op->showLineNumber) printf("%d:", nLines);
-               printf("%s",buffer );
-             }
-           }
-         }
-   }
-   if(op->showNumberOfLines) printf("%d\n",lineCounter);
-
-   free(buffer);
-   fclose(file);
-
-}
-
-
-void updateLogFile(const char *env, char *message) {
-    char *file = getenv(env);
-    FILE *fp = fopen(file, "a+");
-
-    if (fwrite(message, strlen(message), 1, fp) == 0) {
-        fprintf(stderr, "An error occurred while saving to the file\n");
-        exit(1);
+    if (file == NULL) {
+        fprintf(stderr, "could not open: %s\n", filename);
+        exit(2);
     }
 
-    fclose(fp);
+    char *buffer = NULL;
+
+    size_t size = 0;
+
+    char *word_location;
+
+    size_t n = 0;
+
+    int nLines = 0;
+
+    int lineCounter = 0;
+
+
+    if (op->showOnlyFileName) printf("%s\n", filename);
+
+    while ((n = getline(&buffer, &size, file)) != -1) {
+        ++nLines;
+
+        if (((word_location = strstr(buffer, pattern)) != NULL && !op->ignoreCase) ||
+            (op->ignoreCase && (word_location = strcasestr(buffer, pattern)) != NULL)) {
+
+            if (!op->completeWord || completeWord(buffer, pattern, word_location)) {
+                if (op->showNumberOfLines) lineCounter++;
+                else {
+                    if (op->showLineNumber) printf("%d:", nLines);
+                    printf("%s", buffer);
+                }
+            }
+        }
+    }
+    if (op->showNumberOfLines) printf("%d\n", lineCounter);
+
+    free(buffer);
+
+    updateLogFile(env, createFileMessage(getTime(), getpid(), filename, CLOSE));
+    fclose(file);
+
 }
 
 
-void analyse_directory(char *directory, char *pattern,options* op, const char *env) {
+
+
+
+void analyse_directory(char *directory, char *pattern, options *op, const char *env) {
 
     DIR *dirp;
     struct dirent *direntp;
     struct stat stat_buf;
     char *name;
-
 
 
     stat(directory, &stat_buf);
@@ -244,7 +264,7 @@ void analyse_directory(char *directory, char *pattern,options* op, const char *e
      * if so, only do the normal search and exit
      */
     if (S_ISREG(stat_buf.st_mode)) {
-        findPatternFile(directory, pattern,op);
+        findPatternFile(directory, pattern, op, env);
         return;
     }
 
@@ -264,8 +284,8 @@ void analyse_directory(char *directory, char *pattern,options* op, const char *e
         }
 
         if (S_ISREG(stat_buf.st_mode)) {
-            updateLogFile(env, createFileMessage(0.15, getpid(), name));
-            findPatternFile(name, pattern,op);
+            updateLogFile(env, createFileMessage(getTime(), getpid(), name, OPEN));
+            findPatternFile(name, pattern, op, env);
         }
             /*
              * Need to prevent it from opening the current and previous directory again
@@ -320,14 +340,10 @@ void analyse_directory(char *directory, char *pattern,options* op, const char *e
 
 void setLogFile(const char *env) {
     printf(">>What will be name of the logfile?\n");
-    char *name;
+    char *name = NULL;
     size_t size;
 
-
     int n = getline(&name, &size, stdin);
-
-    //removing the trailing newline
-    name[strcspn(name, "\n")] = 0;
 
     if (n == -1) {
         fprintf(stderr, "An error occurred while reading the filename\n");
@@ -336,6 +352,10 @@ void setLogFile(const char *env) {
         fprintf(stderr, "An empty file name is not valid\n");
         setLogFile(env);
     }
+
+    //removing the trailing newline
+    name[strcspn(name, "\n")] = 0;
+
 
     if (setenv(env, name, 1) != 0) {
         fprintf(stderr, "An error occurred while adding the variable\n");
@@ -355,12 +375,12 @@ void reset(options * op){
 }
 
 
-void analyseAction(int argc, char *const argv[], const char* env){
+void analyseAction(int argc, char *const argv[], const char *env) {
 
 
     char *pattern;
 
-    options* op = (options *) malloc(sizeof(options)) ;
+    options *op = (options *) malloc(sizeof(options));
     reset(op);
 
     if (argc == 2) {
@@ -371,15 +391,15 @@ void analyseAction(int argc, char *const argv[], const char* env){
 
     int i;
 
-    for( i = 1; i < argc;i++){
-      if(argv[i][0]== '-') {
-        if(strcmp(argv[i],"-i") == 0){ op->ignoreCase=true;}
-        else if(strcmp(argv[i],"-l")==0)op->showOnlyFileName=true ;
-        else if(strcmp(argv[i],"-n")==0)op->showLineNumber=true ;
-        else if(strcmp(argv[i],"-c")==0)op->showNumberOfLines=true ;
-        else if(strcmp(argv[i],"-w")==0) op->completeWord=true;
-        else if(strcmp(argv[i],"-r")==0) op->recursive=true;
-      }else break;
+    for (i = 1; i < argc; i++) {
+        if (argv[i][0] == '-') {
+            if (strcmp(argv[i], "-i") == 0) { op->ignoreCase = true; }
+            else if (strcmp(argv[i], "-l") == 0)op->showOnlyFileName = true;
+            else if (strcmp(argv[i], "-n") == 0)op->showLineNumber = true;
+            else if (strcmp(argv[i], "-c") == 0)op->showNumberOfLines = true;
+            else if (strcmp(argv[i], "-w") == 0) op->completeWord = true;
+            else if (strcmp(argv[i], "-r") == 0) op->recursive = true;
+        } else break;
     }
 
 
@@ -411,7 +431,7 @@ int main(int argc, char *const argv[]) {
      */
     const char *env = "LOGFILENAME";
 
-    setLogFile(env);
+    gettimeofday(&t_init, NULL);
 
     struct sigaction action;
     sigemptyset(&action.sa_mask);
@@ -439,6 +459,10 @@ int main(int argc, char *const argv[]) {
         exit(1);
     }
 
+    /*
+    * Setting up the env variable
+    */
+    const char *env = "LOGFILENAME";
 
     fprintf(stderr, "Analyse\n");
     analyseAction(argc,argv,env);
